7814a108760836367313eff5f7a3378f1a8b9d55
kam6512 <kam6512@gmail.com>
2020-01-18 01:54:21 +0900

    test root keyfile

--- src/holiday.js
+++ src/holiday.js
@@ -5,11 +5,13 @@ const axios = require('axios');
 const moment = require('./core/moment');
 const res = require('./core/resource');
 
-const filteredEventList = ['식목일', '어버이날', '스승의날', '제헌절', '국군의날', '크리스마스 이브', '섣달 그믐날']
+const filteredEventList = ['식목일', '어버이날', '스승의날', '제헌절', '국군의날', '크리스마스 이브', '섣달 그믐날', '*올림픽']
 const filteredEventKeyword = '림픽'
 
 
 const link = 'https://clients6.google.com/calendar/v3/calendars/ko.south_korea%23holiday@group.v.calendar.google.com/events?calendarId=ko.south_korea%23holiday%40group.v.calendar.google.com&singleEvents=true&timeZone=Asia%2FSeoul&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=$START$-01-01T00%3A00%3A00%2B09%3A00&timeMax=$END$-01-01T00%3A00%3A00%2B09%3A00&key=$KEY$';
+
+
 // Google 공휴일 캘린더 REST API 정보
 const GoogleCalendar = async (year = moment().get('year')) => {
     let key;
