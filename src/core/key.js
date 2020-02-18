eedd55b013c87fd5acfaaab84d7304f376f80b7a
kam6512 <kam6512@gmail.com>
2020-02-19 00:02:19 +0900

    update key js

--- src/core/key.js
+++ src/core/key.js
@@ -1,1 +1,20 @@
 "use strict"
+
+const path = require('path');
+
+const fs = require('fs');
+const fsp = fs.promises;
+const extra = require('fs-extra');
+
+const getGCalendarKey = async () => {
+    let keyPath = path.normalize('calendarKey.json');
+    if (!fs.existsSync(keyPath)) createKeyFile(keyPath);
+    let data = await fsp.readFile(keyPath, 'utf-8');
+    return JSON.parse(data);
+}
+
+const createKeyFile = (keyPath) => {
+    let sourceKeyFile = getFileAbsPath('keyFile.json');
+    let destKeyFile = rootDir + keyPath;
+    extra.copyFileSync(sourceKeyFile, destKeyFile);
+}
