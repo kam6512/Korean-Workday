d62362624752f57d715c75a8483ab61b7cf01218
kam6512 <kam6512@gmail.com>
2020-02-22 00:57:44 +0900

    update  clipboard js

--- src/core/clip.js
+++ src/core/clip.js
@@ -1,2 +1,11 @@
 "use strict"
 const clipboardy = require('clipboardy');
+
+const writeToClip = (source) => {
+    clipboardy.writeSync(JSON.stringify(source));
+    return source;
+}
+
+module.exports = {
+    writeToClip
+}
