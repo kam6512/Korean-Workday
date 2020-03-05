879d2f7d63dce94af33011cc3748c300a069f0c9
kam6512 <kam6512@gmail.com>
2020-03-06 00:42:36 +0900

    update

--- src/resource/key.js
+++ src/resource/key.js
@@ -10,14 +10,14 @@ const { rootDir } = require('../core/context')
 const keyName = 'key.json';
 
 const getGCalendarKey = async () => {
-    let keyPath = path.normalize('key.json');
+    let keyPath = path.normalize(keyName);
     if (!fs.existsSync(keyPath)) createKeyFile(keyPath);
     let data = await fsp.readFile(keyPath, 'utf-8');
     return JSON.parse(data);
 }
 
 const createKeyFile = (keyPath) => {
-    let sourceKeyFile = getFileAbsPath('key.json');
+    let sourceKeyFile = getFileAbsPath(keyName);
     let destKeyFile = rootDir + keyPath;
     extra.copyFileSync(sourceKeyFile, destKeyFile);
 }
